b1b5ee1955bc207bd3e1dadf06b2ecb25cf10668
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2023-02-20 15:31:22 +0200

    Add Feedback.kt, FeedbackReadFragment.kt, fragment_feedback_read.xml. Update new fragments to navigation, activity_main_drawer.xml.

---
+++ app/src/main/java/com/example/em_tuntiesimerkki1/FeedbackReadFragment.kt
@@ -0,0 +1,101 @@
+package com.example.em_tuntiesimerkki1
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.em_tuntiesimerkki1.databinding.FragmentFeedbackReadBinding
+import com.example.em_tuntiesimerkki1.datatypes.feedback.Feedback
+import com.google.gson.GsonBuilder
+import org.json.JSONObject
+
+class FeedbackReadFragment : Fragment() {
+    // change this to match your fragment name
+    private var _binding: FragmentFeedbackReadBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFeedbackReadBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        // the binding -object allows you to access views in the layout, textviews etc.
+        binding.buttonFetchData.setOnClickListener {
+            getFeedback()
+        }
+
+        return root
+    }
+
+    var feedbacks : List<Feedback> = emptyList()
+
+
+    private fun getFeedback() {
+        // this is the url where we want to get our data from
+        val JSON_URL = "https://easnygvt.directus.app/items/feedback?access_token=${BuildConfig.DIRECTUS_ACCESS_TOKEN}"
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+
+        // Request a string response from the provided URL.
+        val stringRequest: StringRequest = object : StringRequest(
+            Request.Method.GET, JSON_URL,
+            Response.Listener { response ->
+                val jsonObject = JSONObject(response)
+                val jsonArray = jsonObject.getJSONArray("data")
+
+                feedbacks = gson.fromJson(jsonArray.toString(), Array<Feedback>::class.java).toList()
+
+                for (item in feedbacks) {
+                    Log.d("ADVTECH", item.name.toString())
+                }
+
+                val adapter = ArrayAdapter(context as Context, android.R.layout.simple_list_item_1, feedbacks)
+                binding.listViewFeekbackDetail.adapter = adapter
+
+                // print the response as a whole
+                // we can use GSON to modify this response into something more usable
+                Log.d("ADVTECH", response)
+
+            },
+            Response.ErrorListener {
+                // typically this is a connection error
+                Log.d("ADVTECH", it.toString())
+            })
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+
+                // basic headers for the data
+                val headers = HashMap<String, String>()
+                headers["Accept"] = "application/json"
+                headers["Content-Type"] = "application/json; charset=utf-8"
+                return headers
+            }
+        }
+
+        // Add the request to the RequestQueue. This has to be done in both getting and sending new data.
+        // if using this in an activity, use "this" instead of "context"
+        val requestQueue = Volley.newRequestQueue(context)
+        requestQueue.add(stringRequest)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/em_tuntiesimerkki1/MainActivity.kt
+++ app/src/main/java/com/example/em_tuntiesimerkki1/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.dataReadFragment,
-                R.id.commentApiFragment, R.id.mapsFragment
+                R.id.commentApiFragment, R.id.mapsFragment, R.id.feedbackReadFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
